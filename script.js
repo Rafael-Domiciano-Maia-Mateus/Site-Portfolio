990e2211cb02615169ff73e6935f96415ada17a3
Rafael-Domiciano-Maia-Mateus <rmaia7985@gmail.com>
2025-07-25 11:32:23 -0300

    Site Portfolio

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function enviarWhats(event) {
     const Name = document.getElementById('name').value;
     const message = document.getElementById('message').value;
     const phone = '5512996186502';
-    const texto = `Olá! Me chamo ${Name}, ${message}`;
+    const texto = `${Name}: ${message}`;
     const formatMessage = encodeURIComponent(texto);
     const url = `https://whatsa.me/${phone}/?t=${formatMessage}`;
 
